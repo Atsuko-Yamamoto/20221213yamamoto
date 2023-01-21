95a128463b5a0724837fd31103966e6438a6755f
Atsuko-Yamamoto <plyhymnia@gmail.com>
2023-01-21 21:42:01 +0900

    correct

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atte</title>
+    <link rel="stylesheet" href="/css/reset.css">
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<header class="header">
+    <h1 class="header_left">Atte</h1>
+</header>
+<main>
+    <div class="user">
+    <h1>ログイン</h1>
+    </div>
+    <div class="form_container">
+    <table>
+        <form action="" method="post">
+            @csrf
+        <tr>
+            <td>
+                <input type="email" name="mail" placeholder="メールアドレス" class="form_box">   
+            </td>
+        </tr>
+            <tr>
+            <td>
+                <input type="password" name="password" placeholder="パスワード" class="form_box">   
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <button type="submit" class="bn_add">ログイン</button>
+            </td>
+        </tr>
+        </form>
+        <tr>
+            <td class="login">
+            <p>アカウントをお持ちでない方はこちらから</p>
+            <a href="{{ route('rest.signup') }}" class="login_link">会員登録</a>
+            </td>
+        </tr>
+    </table>
+</div>
+</main>
+<footer>
+    Atte,inc.
+</footer>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//認証画面の表示
+Route::post('/register', [RestController::class, 'signup'])->name('rest.signup');
+
+//ログイン画面の表示
+Route::get('/login', [RestController::class, 'login'])->name('rest.login');
+
+//一覧画面の表示
+Route::get('/', [RestController::class, 'index'])->name('rest.index')->middleware('auth');
+
+Route::get('/home', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
